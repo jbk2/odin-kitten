3a6436016cfc791c28d9d18b9313f75ffa12243c
jbk2 <james@humanbenefits.com>
2024-01-25 16:12:43 +0100

    Add annotate gem

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: kittens
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  age        :integer
+#  cuteness   :integer
+#  softness   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Kitten < ApplicationRecord
 end
--- spec/models/kitten_spec.rb
+++ spec/models/kitten_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: kittens
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  age        :integer
+#  cuteness   :integer
+#  softness   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Kitten, type: :model do
