c1efa5a346033a3e8bffb902a13eb828a8744310
jbk2 <james@humanbenefits.com>
2024-01-25 18:59:26 +0100

    Manually scaffolded RESTful Kittens
    - created controller with all 7 actions
    - created resource for Kitten routes
    - created Kitten model with 4 columns
    - created views/partials and for all actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,23 +1,51 @@
 class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
   def index
     @kittens = Kitten.all
   end
 
   def show
+    
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = @kitten.new(kitten_params).save
+
+    if @kitten.save
+      redirect_to @kitten, notice: "kitten was successfuly saved"
+    else
+      render new_kitten_path, status: :unprocessable_entity, notice: 'Failed to save kitten to db'
+    end
   end
 
   def edit
+    
   end
 
   def update
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: 'Kitten was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @kitten.destroy
+    redirect_to kittens_path, notice: 'Kitten was successfully destroyed.'
+  end
+  
+  private
+  def kitten_params
+    params.require(:kitten).permit(:id, :name, :age, :cuteness, :softness)
+  end
+
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
   end
 end
